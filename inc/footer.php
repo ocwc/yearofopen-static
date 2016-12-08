a2bf6f8e86e1439e57302f2b026de3879aca02c7
Jure Cuhalev <jure@cuhalev.com>
2016-12-08 16:18:18 +0100

    More footer

--- inc/footer.php
+++ inc/footer.php
@@ -2,16 +2,18 @@
 
       <div class="footer-wrapper">
         <div class="row footer">
-          <div class="small-12 columns"></div>
+            <div class="medium-12 columns">
+                <div class="footer-header">Check out our other events</div>
+            </div>
 
-          <div class="small-2 medium-1 columns footer-license">
-            <a href="#"><img src="/images/cc-by.svg" class="footer-cc-by" /></a>
-          </div>
+            <div class="small-2 medium-1 medium-offset-3 columns footer-license">
+                <a href="#"><img src="/images/cc-by.svg" class="footer-cc-by" /></a>
+            </div>
 
-          <div class="small-10 medium-11 medium-6 columns footer-license">
-            <p>Except where otherwise noted, content on www.yearofopen.org is licensed under a
+            <div class="small-10 medium-5 columns footer-license">
+                <p>Except where otherwise noted, content on www.yearofopen.org is licensed under a
             Creative Commons Attribution 4.0 International License.</p>
-          </div>
+            </div>
         </div>
       </div>
 
